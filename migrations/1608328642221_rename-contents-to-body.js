b1b23662e3f50b09bc0e23ff46c380b6ba0eee15
DmitriyBakhanenko <bahanenko@gmail.com>
2020-12-19 00:08:44 +0200

    removed naming mistake

--- migrations/1608328642221_rename-contents-to-body.js
+++ migrations/1608328642221_rename-contents-to-body.js
@@ -3,14 +3,14 @@
 exports.shorthands = undefined;
 
 exports.up = (pgm) => {
-  pmg.sql(`
+  pgm.sql(`
     ALTER TABLE comments
     RENAME COLUMN contents TO body;
   `);
 };
 
 exports.down = (pgm) => {
-  pmg.sql(`
+  pgm.sql(`
     ALTER TABLE comments
     RENAME COLUMN body TO contents;
   `);
